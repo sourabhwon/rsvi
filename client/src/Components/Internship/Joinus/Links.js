7530e3e235581ea2505b5821ce4f77fd270a53bd
aditiverma01 <79079010+aditiverma01@users.noreply.github.com>
2021-07-01 19:55:20 +0530

    Update Links.js

--- client/src/Components/Internship/Joinus/Links.js
+++ client/src/Components/Internship/Joinus/Links.js
@@ -1,3 +1,3 @@
-const joinUsImg = "https://drive.google.com/thumbnail?id=1sBAQlkRwfZBnRzmTjMux_eO3XxKsLDHL";
+const joinUsImg = "https://drive.google.com/thumbnail?id=14-F7_GWIMXBKg1wiyhaW9Zb2jGdL_PJo";
 
 export default joinUsImg;
